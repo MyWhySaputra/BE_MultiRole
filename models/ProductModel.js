7d89b2821458df2312717be25c1d377d6531cddb
MyWahyuSap <wahyusaputra222000@gmail.com>
2024-01-30 16:26:25 +0700

    create model 30/01/2024

---
+++ models/ProductModel.js
@@ -0,0 +1,49 @@
+const Sequelize = require("sequelize");
+const db = require("../config/Database_MySQL.js");
+const Users = require("./UserModel.js");
+
+const { DataTypes } = Sequelize;
+
+const Products = db.define(
+  "product",
+  {
+    uuid: {
+      type: DataTypes.STRING,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        len: [3, 100],
+      },
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+  },
+  {
+    freezeTableName: true,
+  }
+);
+
+Users.hasMany(Products);
+Products.belongsTo(Users, { foreignKey: "userId" });
+
+module.exports = Products;
