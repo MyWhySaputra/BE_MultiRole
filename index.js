009bdd98c908969b5fdb7234fab5d49038d27a6b
MyWahyuSap <wahyusaputra222000@gmail.com>
2024-01-30 16:25:01 +0700

    update index 30/01/2024

--- index.js
+++ index.js
@@ -2,8 +2,42 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const session = require("express-session");
+const db = require("./config/Database_MySQL");
+const SequelizeStore = require("connect-session-sequelize");
+const router = require("./routes/routes");
 
 const app = express();
+
+const sessionStore = SequelizeStore(session.Store);
+
+const store = new sessionStore({
+  db: db,
+});
+
+app.use(
+  session({
+    secret: process.env.SESS_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: store,
+    cookie: {
+      secure: "auto",
+    },
+  })
+);
+
+app.use(
+  cors({
+    credentials: true, // untuk fontend mengirimkan cookie
+    origin: "http://localhost:3000", // alamat frontend yang diizinkan
+  })
+);
+
+app.use(express.json());
+
+app.use("/", router);
+
+// store.sync();
 
 const port = process.env.PORT || 3000;
 
